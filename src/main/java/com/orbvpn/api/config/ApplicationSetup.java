941b92630fb1232a615cc625c7305e84753fe574
ValloonSweet <valloon91105@gmail.com>
2021-12-06 23:13:52 +0000

    reload nginx on application start after file refreshing

--- src/main/java/com/orbvpn/api/config/ApplicationSetup.java
+++ src/main/java/com/orbvpn/api/config/ApplicationSetup.java
@@ -1,5 +1,6 @@
 package com.orbvpn.api.config;
 
+import com.orbvpn.api.service.common.ShellCodeUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,10 @@ public class ApplicationSetup {
 
     @PostConstruct
     public void refreshNginxFile() {
+        log.debug("refreshing nginx...");
+        /**
+         * Step1: create refresh file for nginx
+         */
         Resource resource = resourceLoader
                 .getResource("classpath:deployment/" + nginxFileName);
         if (resource.exists()) {
@@ -44,12 +49,25 @@ public class ApplicationSetup {
                     destFile.createNewFile();
                 }
                 FileCopyUtils.copy(resource.getFile(), destFile);
-                log.info("NGINX.CONG.txt is refresh.");
+                log.info(nginxFileName + " file is refresh.");
             } catch (IOException e) {
                 log.error("Failed to create " + nginxFileName + " file in " + nginxDestinationPath);
+                return;
             }
         } else {
             log.error("NGINX.CONG.txt resource doesn't exist and refreshing Nginx file is not done.");
+            return;
+        }
+        /**
+         * Step2: reloading nginx server
+         */
+        String command = "sudo systemctl reload nginx";
+        try {
+            ShellCodeUtil.ShellCodeExecutionResult result = ShellCodeUtil.runShellCode(new String[]{command});
+            log.debug("nginx reload executed with result of: " + result);
+        } catch (IOException | InterruptedException e) {
+            log.error("error in reloading nginx", e);
+            return;
         }
     }
 
