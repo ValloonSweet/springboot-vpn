9c6d5e2cc270d38f7f8f6b50de8c460a95d0bece
ValloonSweet <valloon91105@gmail.com>
2021-12-06 20:58:32 +0000

    refreshing NGINX.CONG.txt file in etc/nginx/ path on every project setup

--- src/main/java/com/orbvpn/api/config/ApplicationSetup.java
+++ src/main/java/com/orbvpn/api/config/ApplicationSetup.java
@@ -2,10 +2,16 @@ package com.orbvpn.api.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.IOException;
 import java.util.TimeZone;
 
 @Service
@@ -14,6 +20,38 @@ import java.util.TimeZone;
 public class ApplicationSetup {
     @Value("${application.default-timezone}")
     private String defaultTimeZone;
+
+    @Value("${application.nginx.file-name}")
+    private String nginxFileName;
+
+    @Value("${application.nginx.destination-path}")
+    private String nginxDestinationPath;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @PostConstruct
+    public void refreshNginxFile() {
+        Resource resource = resourceLoader
+                .getResource("classpath:deployment/" + nginxFileName);
+        if (resource.exists()) {
+            try {
+                File destFile = new File(nginxDestinationPath + nginxFileName);
+                if (!destFile.getParentFile().exists()) {
+                    destFile.getParentFile().mkdirs();
+                }
+                if (!destFile.exists()) {
+                    destFile.createNewFile();
+                }
+                FileCopyUtils.copy(resource.getFile(), destFile);
+                log.info("NGINX.CONG.txt is refresh.");
+            } catch (IOException e) {
+                log.error("Failed to create " + nginxFileName + " file in " + nginxDestinationPath);
+            }
+        } else {
+            log.error("NGINX.CONG.txt resource doesn't exist and refreshing Nginx file is not done.");
+        }
+    }
 
     @PostConstruct
     public void setDefaultTimezone() {
