9c23b08cf67ea33d86f415fb78ac79db593dba3a
ValloonSweet <valloon91105@gmail.com>
2022-01-22 03:11:14 +0900

    \

--- src/main/java/com/orbvpn/api/config/ApplicationSetup.java
+++ src/main/java/com/orbvpn/api/config/ApplicationSetup.java
@@ -37,48 +37,48 @@ public class ApplicationSetup {
     @Autowired
     private ResourceLoader resourceLoader;
 
-    @PostConstruct
-    public void refreshNginxFile() {
-        log.debug("refreshing nginx...");
-        /**
-         * Step1: create refresh file for nginx
-         */
-        Resource resource = resourceLoader
-                .getResource("classpath:deployment/" + nginxFileName);
-        if (resource.exists()) {
-            try {
-                File destFile = new File(nginxDestinationPath + nginxFileName);
-                if (!destFile.getParentFile().exists()) {
-                    destFile.getParentFile().mkdirs();
-                }
-                if (!destFile.exists()) {
-                    destFile.createNewFile();
-                }
-                FileCopyUtils.copy(resource.getFile(), destFile);
-                log.info(nginxFileName + " file is refresh.");
-            } catch (IOException e) {
-                log.error("Failed to create " + nginxFileName + " file in " + nginxDestinationPath);
-                return;
-            }
-        } else {
-            log.error("NGINX.CONG.txt resource doesn't exist and refreshing Nginx file is not done.");
-            return;
-        }
-        /**
-         * Step2: reloading nginx server
-         */
-        if (SystemUtils.IS_OS_LINUX) {
-            log.debug("reloading nginx...");
-            String[] commands = { "sudo", "systemctl", "reload", "nginx" };
-            try {
-                ShellCodeUtil.ShellCodeExecutionResult result = ShellCodeUtil.runShellCode(commands);
-                log.debug("nginx reload executed with result of: " + result);
-            } catch (IOException | InterruptedException e) {
-                log.error("error in reloading nginx", e);
-                return;
-            }
-        }
-    }
+    // @PostConstruct
+    // public void refreshNginxFile() {
+    //     log.debug("refreshing nginx...");
+    //     /**
+    //      * Step1: create refresh file for nginx
+    //      */
+    //     Resource resource = resourceLoader
+    //             .getResource("classpath:deployment/" + nginxFileName);
+    //     if (resource.exists()) {
+    //         try {
+    //             File destFile = new File(nginxDestinationPath + nginxFileName);
+    //             if (!destFile.getParentFile().exists()) {
+    //                 destFile.getParentFile().mkdirs();
+    //             }
+    //             if (!destFile.exists()) {
+    //                 destFile.createNewFile();
+    //             }
+    //             FileCopyUtils.copy(resource.getFile(), destFile);
+    //             log.info(nginxFileName + " file is refresh.");
+    //         } catch (IOException e) {
+    //             log.error("Failed to create " + nginxFileName + " file in " + nginxDestinationPath);
+    //             return;
+    //         }
+    //     } else {
+    //         log.error("NGINX.CONG.txt resource doesn't exist and refreshing Nginx file is not done.");
+    //         return;
+    //     }
+    //     /**
+    //      * Step2: reloading nginx server
+    //      */
+    //     if (SystemUtils.IS_OS_LINUX) {
+    //         log.debug("reloading nginx...");
+    //         String[] commands = { "sudo", "systemctl", "reload", "nginx" };
+    //         try {
+    //             ShellCodeUtil.ShellCodeExecutionResult result = ShellCodeUtil.runShellCode(commands);
+    //             log.debug("nginx reload executed with result of: " + result);
+    //         } catch (IOException | InterruptedException e) {
+    //             log.error("error in reloading nginx", e);
+    //             return;
+    //         }
+    //     }
+    // }
 
     @PostConstruct
     public void setDefaultTimezone() {
