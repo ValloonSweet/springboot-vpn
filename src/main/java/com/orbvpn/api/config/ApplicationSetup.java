a613f899ce412a81ff07126e694bf168039d9e52
ValloonSweet <valloon91105@gmail.com>
2022-01-24 19:54:39 +0300

    https -> http

--- src/main/java/com/orbvpn/api/config/ApplicationSetup.java
+++ src/main/java/com/orbvpn/api/config/ApplicationSetup.java
@@ -1,22 +1,13 @@
 package com.orbvpn.api.config;
 
-// import java.io.File;
-// import java.io.IOException;
 import java.util.TimeZone;
 
 import javax.annotation.PostConstruct;
 
-// import com.orbvpn.api.service.common.ShellCodeUtil;
-
-// import org.apache.commons.lang3.SystemUtils;
-// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-// import org.springframework.core.io.Resource;
-// import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
-// import org.springframework.util.FileCopyUtils;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,58 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 public class ApplicationSetup {
     @Value("${application.default-timezone}")
     private String defaultTimeZone;
-
-    // @Value("${application.nginx.file-name}")
-    // private String nginxFileName;
-
-    // @Value("${application.nginx.destination-path}")
-    // private String nginxDestinationPath;
-
-    // @Autowired
-    // private ResourceLoader resourceLoader;
-
-    // @PostConstruct
-    // public void refreshNginxFile() {
-    //     log.debug("refreshing nginx...");
-    //     /**
-    //      * Step1: create refresh file for nginx
-    //      */
-    //     Resource resource = resourceLoader
-    //             .getResource("classpath:deployment/" + nginxFileName);
-    //     if (resource.exists()) {
-    //         try {
-    //             File destFile = new File(nginxDestinationPath + nginxFileName);
-    //             if (!destFile.getParentFile().exists()) {
-    //                 destFile.getParentFile().mkdirs();
-    //             }
-    //             if (!destFile.exists()) {
-    //                 destFile.createNewFile();
-    //             }
-    //             FileCopyUtils.copy(resource.getFile(), destFile);
-    //             log.info(nginxFileName + " file is refresh.");
-    //         } catch (IOException e) {
-    //             log.error("Failed to create " + nginxFileName + " file in " + nginxDestinationPath);
-    //             return;
-    //         }
-    //     } else {
-    //         log.error("NGINX.CONG.txt resource doesn't exist and refreshing Nginx file is not done.");
-    //         return;
-    //     }
-    //     /**
-    //      * Step2: reloading nginx server
-    //      */
-    //     if (SystemUtils.IS_OS_LINUX) {
-    //         log.debug("reloading nginx...");
-    //         String[] commands = { "sudo", "systemctl", "reload", "nginx" };
-    //         try {
-    //             ShellCodeUtil.ShellCodeExecutionResult result = ShellCodeUtil.runShellCode(commands);
-    //             log.debug("nginx reload executed with result of: " + result);
-    //         } catch (IOException | InterruptedException e) {
-    //             log.error("error in reloading nginx", e);
-    //             return;
-    //         }
-    //     }
-    // }
 
     @PostConstruct
     public void setDefaultTimezone() {
