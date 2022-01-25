bd8a140b253f6b994d4dff1527687f46cb68bf44
ValloonSweet <valloon91105@gmail.com>
2022-01-25 19:39:32 +0300

    Notifications redesigned.

--- src/main/java/com/orbvpn/api/service/NotificationService.java
+++ src/main/java/com/orbvpn/api/service/NotificationService.java
@@ -30,15 +30,15 @@ public class NotificationService {
     private final SmsService smsService;
 
     /**
-     * send Sms and Email at 8 AM London time to people whose birthday are today.
+     * Send Sms and Email at 8 AM London time to people whose birthday are today.
      */
     @Scheduled(cron = EVERY_DAY_8AM)
     public void sendBirthdayWish() {
         log.info("sending birthday notifications...");
-        String smsMessage = "Happy birthday!\nMay you live your dreams.\nWith lots of love.\nOrbVPN";
+        String smsMessage = "Happy birthday!\nMay you live your dreams.\nWith lots of love,\nOrbVPN";
         String emailTitle = "OrbVPN: Happy Birthday!";
-        String emailMessage = "From your friends at <b>OrbVPN</b>,<br>" +
-                "We wish you a fabulous birthday celebration.<br>" +
+        String emailMessage = "From your friends at <b>OrbVPN</b>,<br><br>" +
+                "We wish you a fabulous birthday celebration.<br><br>" +
                 "May the days ahead of you be filled with prosperity, great health and above all joy in " +
                 "its truest and purest form.";
 
@@ -50,7 +50,7 @@ public class NotificationService {
     }
 
     /**
-     * sending subscription expiration reminders 1, 5 and 10 day(s) before expiration at 8 AM London time.
+     * Sending subscription expiration reminders 1, 5 and 10 day(s) before expiration at 8 AM London time.
      */
     @Scheduled(cron = EVERY_DAY_8AM)
     public void subscriptionExpirationReminder() {
@@ -64,12 +64,15 @@ public class NotificationService {
                 String emailTitle = "OrbVPN: Subscription Expiration Reminder";
                 String emailMessage = "There is just " + dayCount + " " +
                         (dayCount == 1 ? "day" : "days") +
-                        " left until your OrbVPN subscription expires. <br>" +
-                        "To renew your account please " +
-                        letUsKnow("Renew account order for available subscription",
-                                "Please renew my account that there is just " + dayCount + " " +
-                                        (dayCount == 1 ? "day" : "days") +
-                                        " left until my OrbVPN subscription expires.");
+                        " left until your OrbVPN subscription expires. <br><br>" +
+                        "To renew your account please contact us on Whatsapp https://wa.me/message/3NYJBB6MNCQPM1 " +
+                        "or via " +
+                        mailtoMessage("email",
+                                "Renew account order for available subscription",
+                            "Please renew my account that there is just " + dayCount + " " +
+                                (dayCount == 1 ? "day" : "days") +
+                                " left until my OrbVPN subscription expires.") + ".";
+
                 sendSms(userProfile, smsMessage);
                 sendEmail(userProfile, emailTitle, emailMessage);
             }
@@ -77,20 +80,26 @@ public class NotificationService {
         log.info("sent subscription expiration reminders successfully");
     }
 
+    /**
+     * Sending after subscription expiration reminders 1, 5 and 10 day(s) before expiration at 8 AM London time.
+     */
     @Scheduled(cron = EVERY_DAY_8AM)
     public void afterSubscriptionExpiredNotification() {
         log.info("sending notification after subscription expiration after expiration...");
         for (Integer dayCount : DAYS_AFTER_EXPIRATION) {
             List<UserProfile> userProfiles = userSubscriptionService.getUsersExpireInPreviousDays(dayCount);
             for (UserProfile userProfile : userProfiles) {
-                String smsMessage = "Your subscription has expired " + dayCount + " " +
+                String smsMessage = "Your OrbVPN subscription has expired " + dayCount + " " +
                         (dayCount == 1 ? "day" : "days") +
-                        " ago. Please don't hesitate to contact us if you would like to renew your service.";
+                        " ago. Please contact us to renew your service.\n";
                 String emailTitle = "OrbVPN: Subscription Expired";
                 String emailMessage = "Your subscription has expired " + dayCount + " " +
                         (dayCount == 1 ? "day" : "days") +
-                        " ago. Please don't hesitate to " +
-                        letUsKnow("Renew account order for expired subscription",
+                        " ago.<br><br> Please contact us to renew your service.<br>" +
+                        "Whatsapp: https://wa.me/message/3NYJBB6MNCQPM1" +
+                        "Email : " +
+                        mailtoMessage("info@orbvpn.com",
+                                "Renew account order for expired subscription",
                                 "Please renew my account. My subscription has expired " + dayCount + " " +
                                         (dayCount == 1 ? "day" : "days") + " ago.") + " if you would like to renew your service.";
                 sendSms(userProfile, smsMessage);
@@ -103,7 +112,7 @@ public class NotificationService {
     /**
      * Welcome new customers for joining ORB-VPN via SMS and Email
      *
-     * @param user new created user
+     * @param user New created user
      */
     public void welcomingNewUsers(User user) {
         String smsMessage = "Welcome to OrbVPN.\n" +
@@ -124,6 +133,12 @@ public class NotificationService {
         sendEmail(user.getProfile(), emailTitle, emailMessage);
     }
 
+    /**
+     * Reset password notifications
+     *
+     * @param user User to change password
+     * @param token Generated token for user
+     */
     public void resetPassword(User user, String token) {
         String smsMessage = "Your OrbVPN password reset code is : " + token;
         String emailTitle = "OrbVPN: Password Reset Code";
@@ -134,19 +149,25 @@ public class NotificationService {
                 "<strong>Code:</strong> " + token + "<br><br>" +
                 "If you ignore this message your password won't be changed.<br>" +
                 "If you didn't request a password reset, please " +
-                letUsKnow("wrong reset password request",
+                mailtoMessage("let us know","wrong reset password request",
                         "this reset password reset request is not from me.") + ".";
 
         sendSms(user.getProfile(), smsMessage);
         sendEmail(user.getProfile(), emailTitle, emailMessage);
     }
 
+    /**
+     * Changing password is done notifications
+     *
+     * @param user User with changed password
+     */
     public void resetPasswordDone(User user) {
-        String smsMessage = "Your password is reset successfully.";
-        String emailTitle = "OrbVPN: Successful Password Reset";
-        String emailMessage = "Your password has been changed successfully<br><br>" +
+        String smsMessage = "Your OrbVPN password is changed.";
+        String emailTitle = "OrbVPN: Your Password Changed";
+        String emailMessage = "Your password has been changed successfully.<br><br>" +
                 "If you didn't request a password reset, please " +
-                letUsKnow("Wrong Successful Reset Password",
+                mailtoMessage("let us know",
+                        "Wrong Successful Reset Password",
                         "This reset password that has been done is not from me.") + ".";
         sendSms(user.getProfile(), smsMessage);
         sendEmail(user.getProfile(), emailTitle, emailMessage);
@@ -165,6 +186,12 @@ public class NotificationService {
         sendEmail(user.getProfile(), emailTitle, emailMessage);
     }
 
+    /**
+     * Sending an SMS to the user
+     *
+     * @param userProfile User profile to send SMS
+     * @param message Message to be sent to the user
+     */
     private void sendSms(UserProfile userProfile, String message) {
         String starting;
         if (userProfile != null && userProfile.getFirstName() != null) {
@@ -179,6 +206,13 @@ public class NotificationService {
         }
     }
 
+    /**
+     * Sending an e-mail to the user
+     *
+     * @param userProfile User profile to send e-mail
+     * @param title The title of the email to be sent
+     * @param message Message to be sent to the user
+     */
     private void sendEmail(UserProfile userProfile, String title, String message) {
         if (userProfile == null)
             return;
@@ -220,10 +254,17 @@ public class NotificationService {
                 emailHtml);
     }
 
-    private String letUsKnow(String title, String message) {
+    /**
+     * Method used to embed the mailto attribute
+     *
+     * @param text The text that the mailto property will display
+     * @param title The title prepared by mailto feature
+     * @param message Message prepared by mailto feature
+     */
+    private String mailtoMessage(String text, String title, String message) {
         title = title.replace(" ", "%20");
         message = message.replace(" ", "%20");
         return "<a href = \"mailto:info@orbvpn.com?subject=" + title + "&body=" + message + "\">\n" +
-                "let us know</a>";
+                 text + "</a>";
     }
 }
