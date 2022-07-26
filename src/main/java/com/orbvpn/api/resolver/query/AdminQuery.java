76e2e74ab3a9b13bee85f4d95fb61b5e01add62f
ValloonSweet <valloon91105@gmail.com>
2022-07-26 21:19:05 +0800

    add: get user by id

--- src/main/java/com/orbvpn/api/resolver/query/AdminQuery.java
+++ src/main/java/com/orbvpn/api/resolver/query/AdminQuery.java
@@ -1,17 +1,28 @@
 package com.orbvpn.api.resolver.query;
 
 import com.orbvpn.api.domain.dto.UserView;
+import com.orbvpn.api.mapper.UserViewMapper;
 import com.orbvpn.api.service.AdminService;
+import com.orbvpn.api.service.UserService;
+
 import graphql.kickstart.tools.GraphQLQueryResolver;
 import lombok.RequiredArgsConstructor;
+
+import javax.annotation.security.RolesAllowed;
+
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
+
+import static com.orbvpn.api.domain.enums.RoleName.Constants.ADMIN;
+
 
 @Component
 @RequiredArgsConstructor
 public class AdminQuery implements GraphQLQueryResolver {
 
     private final AdminService adminService;
+    private final UserService userService;
+    private final UserViewMapper userViewMapper;
 
     public Page<UserView> activeUsers(int page, int size) {
         return adminService.getActiveUsers(page, size);
@@ -28,4 +39,11 @@ public class AdminQuery implements GraphQLQueryResolver {
     public int totalActiveUsers() {
         return adminService.getTotalActiveUsers();
     }
+
+    @RolesAllowed(ADMIN)
+    public UserView getUserById(int id) {
+        var user = userService.getUserById(id);
+        return userViewMapper.toView(user);
+    }
+
 }
