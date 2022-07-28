27f9cc2cbb4ba05699459c048b419edff02b90b5
ValloonSweet <valloon91105@gmail.com>
2022-07-28 09:52:22 +0300

    news, reseller updated

--- src/main/java/com/orbvpn/api/mapper/NewsViewMapper.java
+++ src/main/java/com/orbvpn/api/mapper/NewsViewMapper.java
@@ -3,9 +3,14 @@ package com.orbvpn.api.mapper;
 import com.orbvpn.api.domain.dto.NewsView;
 import com.orbvpn.api.domain.entity.News;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
 
 @Mapper(componentModel = "spring", uses = RoleViewMapper.class)
 public interface NewsViewMapper {
 
+  @Mappings({
+    @Mapping(source = "sendMail", target = "notifyByEmail"),
+  })
   NewsView toView(News news);
 }
